3537ca26d6b8143e3dbbd648219fef360fc8ecc4
Umairramaay <59950428+Umairramaay@users.noreply.github.com>
2020-07-08 15:55:17 +0500

    Update settings.py

--- ms/settings.py
+++ ms/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j=f%1v!r_15h^((u8brml$)38bm2zbvz3thc3ggvzwu_)$2%d7'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -150,10 +150,10 @@ EMAIL_FILE_PATH=os.path.join(BASE_DIR,"sent_mails")
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST="smtp.gmail.com"
 # EMAIL_PORT=587
-# EMAIL_HOST_USER="umairshabr@gmail.com"
-# EMAIL_HOST_PASSWORD="ramay@1122"
+# EMAIL_HOST_USER="YOUR MAIL"
+# EMAIL_HOST_PASSWORD="YOUR PASSWORD"
 # EMAIL_USE_TLS=True
-# DEFAULT_FROM_EMAIL="Student management System <umairshabr@gmail.com>"
+# DEFAULT_FROM_EMAIL="Student management System <MAIL>"
 
 import dj_database_url
 prod_db=dj_database_url.config(conn_max_age=500)
